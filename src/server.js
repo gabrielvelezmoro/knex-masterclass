83c07f128f3f08abdefebda28f968d665e9072d0
gabri <gabrieuvelezmoro@gmail.com>
2023-05-21 09:51:56 -0300

    feat: list users

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 const express =  require ('express');
+const knex = require('./database');
 const app = express()
 require('dotenv').config();
+
+app.get('/users', (req, res) => {
+    knex('users').then((results)=>{
+        return res.json(results)
+    })
+})
 
 app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`))
