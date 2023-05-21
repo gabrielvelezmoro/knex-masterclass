32cb9c2b9faaee4c1421cc5f8bbae3b397ea1866
gabri <gabrieuvelezmoro@gmail.com>
2023-05-21 09:47:15 -0300

    chore: make seeds

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,11 @@ module.exports = {
     migrations: {
       tableName: 'knex_migrations',
       directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
     }
+    
   },
 
 };
